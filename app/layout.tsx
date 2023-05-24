4ba10b83cea9e3cc88afe4f5c5a2f7f57b984cdf
Celestine03 <celestinet03@gmail.com>
2023-05-24 23:40:41 +0800

    added dark and light mode toggle

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
+"use client"
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Navbar from '@/components/navbar'
 import Footer from '@/components/footer'
+import { ThemeProvider } from 'next-themes'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,12 +18,16 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Navbar />
-        {children}
-        <Footer />
-      </body>
-    </html>
+    
+      <html lang="en">
+        <body className={inter.className}>
+          <ThemeProvider attribute="class">
+            <Navbar />
+            {children}
+            <Footer />
+          </ThemeProvider>
+        </body> 
+      </html>
+    
   )
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,19 +1,48 @@
 import Link from "next/link"
+import {useTheme} from "next-themes";
+import Image from "next/image";
+import {useState, useEffect} from "react";
 
 export default function Navbar() {
+
+  const themeSwitch = () => {
+    const [mounted, setMounted] = useState(false);
+    const {theme, setTheme} = useTheme();
+
+    useEffect(() => {
+      setMounted(true)
+    }, [])
+
+    if (!mounted) {
+      return null
+    }
+
+    return (
+      <Image className="mx-20 h-auto w-auto"
+        role="button"
+        onClick={() => setTheme(theme === "dark" ? 'light' : 'dark')}
+        src={theme === "dark" ? "/sun-icon.png" : "/moon-icon.png"}
+        alt={theme === "dark" ? "sun icon" : "moon icon"}
+        width={25}
+        height={10}
+      />
+    )
+  }
+
   return (
     <nav className="bg-blue-300 dark:bg-slate-800 p-5 sticky top-0 z-10">
       <div className="prose lg:prose-xl flex flex-row sm:flex-auto space-x-4 justify-end">
-        <Link href="" className="">
+        <Link href="">
           <p className="flex text-xl font-semibold px-4 rounded-full border border-transparent item-center transition-colors hover:border-gray-400 hover:bg-white hover:text-blue-300 hover:dark:border-neutral-700 hover:dark:bg-neutral-500/30 hover:dark:text-white">
             Home
           </p>
         </Link>
-        <Link href="" className="">
+        <Link href="">
           <p className="flex text-xl font-semibold px-4 rounded-full border border-transparent item-center transition-colors hover:border-gray-400 hover:bg-white hover:text-blue-300 hover:dark:border-neutral-700 hover:dark:bg-neutral-500/30 hover:dark:text-white">
             Thoughts
           </p>
         </Link>
+        {themeSwitch()}
       </div>
     </nav>
   )
