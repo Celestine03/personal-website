15aeb21713a8945b3d053880315ee36dce867b3e
Celestine03 <celestinet03@gmail.com>
2023-05-24 20:52:18 +0800

    added footer

---
+++ components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-blue-300 dark:bg-slate-800 py-1">
+      <p className="text-center">© 2023 Celestine Tan</p>
+    </footer>
+  )
+}
